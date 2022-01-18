4ac8eaa2f93a57ef252cdbd1c04162865feea756
catfishjw <james.wiley@live.com>
2022-01-18 15:12:05 -0600

    fishingController
    fish can now spawn, ui management, scoring, win condition will all be added later

--- Assets/scripts/fishingController.cs
+++ Assets/scripts/fishingController.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class fishingController : MonoBehaviour
 {
+    public string[] buzzwords;
+    public int[] fishValues;
+    public int timer;
+
+    public float spawnTime = 5f;
+    public float spawnDelay = 3f;
+    public GameObject myPrefab;
+    public GameObject canvasObject;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("Spawn", spawnDelay, spawnTime);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void Spawn()
+    {
+        // Instantiate a random prefab
+        int spawnX = Random.Range(-69, 600);
+        int spawnY = Random.Range(0, 560);
+        GameObject childObject = Instantiate(myPrefab, new Vector3(spawnX, spawnY, 0), transform.rotation) as GameObject;
+        childObject.transform.parent = canvasObject.transform;
     }
 }
